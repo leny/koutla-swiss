c7c3e13b3b99fe91905c4f810253d07229c36577
leny <info@flatland.be>
2018-01-26 12:02:24 +0100

    Implement minSize & maxSize

--- src/mixins/size.js
+++ src/mixins/size.js
@@ -6,7 +6,15 @@
  * started at 13/01/2018
  */
 
-export const size = (mWidth = 0, mHeight = mWidth) => ({
-    width: `${mWidth}`,
-    height: `${mHeight}`,
+const _size = (iWidth, iHeight, sPrefix = "") => ({
+    [`${sPrefix}${sPrefix !== "" ? "Width" : "width"}`]: iWidth,
+    [`${sPrefix}${sPrefix !== "" ? "Height" : "height"}`]: iHeight,
 });
+
+export const size = (mWidth = 0, mHeight = mWidth) => _size(mWidth, mHeight);
+
+export const minSize = (mWidth = 0, mHeight = mWidth) =>
+    _size(mWidth, mHeight, "min");
+
+export const maxSize = (mWidth = 0, mHeight = mWidth) =>
+    _size(mWidth, mHeight, "max");
--- test/mixins.size.js
+++ test/mixins.size.js
@@ -8,7 +8,7 @@
 
 import assert from "assert";
 
-import {size} from "../lib/mixins";
+import {size, minSize, maxSize} from "../lib/mixins";
 
 describe("Size Mixins", () => {
     describe("size()", () => {
@@ -32,6 +32,72 @@ describe("Size Mixins", () => {
 
         it("Should return 0 when no param are given", () => {
             assert.deepEqual(size(), {width: "0", height: "0"});
+        });
+    });
+
+    describe("minSize()", () => {
+        it("Should return min-width & min-height when two params are given", () => {
+            assert.deepEqual(minSize("1rem", "0.5rem"), {
+                minWidth: "1rem",
+                minHeight: "0.5rem",
+            });
+            assert.deepEqual(minSize("100%", "5rem"), {
+                minWidth: "100%",
+                minHeight: "5rem",
+            });
+            assert.deepEqual(minSize("100%", 0), {
+                minWidth: "100%",
+                minHeight: "0",
+            });
+        });
+
+        it("Should return same min-width & min-height when one param is given", () => {
+            assert.deepEqual(minSize("1rem"), {
+                minWidth: "1rem",
+                minHeight: "1rem",
+            });
+            assert.deepEqual(minSize("100%"), {
+                minWidth: "100%",
+                minHeight: "100%",
+            });
+            assert.deepEqual(minSize(0), {minWidth: "0", minHeight: "0"});
+        });
+
+        it("Should return 0 when no param are given", () => {
+            assert.deepEqual(minSize(), {minWidth: "0", minHeight: "0"});
+        });
+    });
+
+    describe("maxSize()", () => {
+        it("Should return max-width & max-height when two params are given", () => {
+            assert.deepEqual(maxSize("1rem", "0.5rem"), {
+                maxWidth: "1rem",
+                maxHeight: "0.5rem",
+            });
+            assert.deepEqual(maxSize("100%", "5rem"), {
+                maxWidth: "100%",
+                maxHeight: "5rem",
+            });
+            assert.deepEqual(maxSize("100%", 0), {
+                maxWidth: "100%",
+                maxHeight: "0",
+            });
+        });
+
+        it("Should return same max-width & max-height when one param is given", () => {
+            assert.deepEqual(maxSize("1rem"), {
+                maxWidth: "1rem",
+                maxHeight: "1rem",
+            });
+            assert.deepEqual(maxSize("100%"), {
+                maxWidth: "100%",
+                maxHeight: "100%",
+            });
+            assert.deepEqual(maxSize(0), {maxWidth: "0", maxHeight: "0"});
+        });
+
+        it("Should return 0 when no param are given", () => {
+            assert.deepEqual(maxSize(), {maxWidth: "0", maxHeight: "0"});
         });
     });
 });
